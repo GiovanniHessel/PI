7b673e6c5bcd78e5c44a07f8226f82e089ef27c9
GiovanniHessel <giovanni.hphessel@gmail.com>
2018-12-06 00:56:09 -0200

    -Incluido um serviço de Estoque

---
+++ pi/src/main/java/sp/com/senac/pi/servicos/producao/ServicoEstoque.java
@@ -0,0 +1,74 @@
+package sp.com.senac.pi.servicos.producao;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import sp.com.senac.pi.model.dao.producao.EstoqueDao;
+import sp.com.senac.pi.model.pojo.producao.Estoque;
+import sp.com.senac.pi.model.pojo.producao.Produto;
+
+@Path("/estoque")
+public class ServicoEstoque {
+	
+	@POST
+	@Path("id")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Estoque getEstoqueId(Estoque estoque) {
+		
+		return new EstoqueDao().getEstoque(estoque.getId());
+	}
+	
+	@POST
+	@Path("produto")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Estoque> getEstoques(Produto produto) {
+		
+		return new EstoqueDao().getEstoques(produto);
+	}
+	
+	@GET
+	@Path("lista")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Estoque> getEstoques() {
+		
+		return new EstoqueDao().getEstoques();
+	}
+	
+	@POST
+	@Path("inserir")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Estoque insert(Estoque estoque) {
+		
+		return new EstoqueDao().insert(estoque);
+	}
+	
+	@PUT
+	@Path("alterar")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Estoque update(Estoque estoque) {
+		
+		return new EstoqueDao().update(estoque);
+	}
+	
+	@DELETE
+	@Path("deletar")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Estoque delete(Estoque estoque) {
+		
+		return new EstoqueDao().delete(estoque);
+	}
+	
+}
